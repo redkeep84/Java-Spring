f7588d7ed9eefd0a6b9edee39a632058ad981884
김창령 <ckdfud@gmail.com>
2023-05-30 12:38:57 +0900

    20.배열(arry)

---
+++ Chapter2/src/ch20/dArr.java
@@ -0,0 +1,34 @@
+package ch20;
+
+public class dArr {
+
+	public static void main(String[] args) {
+
+//		double[] dArr = new double[5];
+//		
+//		dArr[0] = 1.1;
+//		dArr[1] = 2.1;
+//		dArr[2] = 3.1;
+//		
+//		double mtotal = 1;
+//		for(int i = 0; i< dArr.length; i++) {
+//			mtotal *= dArr[i];
+//		}
+//		
+//		System.out.println(mtotal);
+		
+		double[] dArr = new double[5];
+		int count = 0;
+		dArr[0] = 1.1; count++;
+		dArr[1] = 2.1; count++;
+		dArr[2] = 3.1; count++;
+		
+		double mtotal = 1;
+		for(int i =0; i< count; i++) {
+			mtotal *= dArr[i];
+		}
+		
+		System.out.println(mtotal);
+	}
+
+}
