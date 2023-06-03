b4b2d611fd98773dc724b85a8ee14900ce6076ae
김창령 <ckdfud@gmail.com>
2023-06-03 22:31:42 +0900

    06. 다형성과 다형성을 사용하는 이유

---
+++ Chapter3/src/ch06/VIPCustomer.java
@@ -0,0 +1,37 @@
+package ch06;
+
+public class VIPCustomer extends Customer{
+
+	double salesRatio;
+	private String agentID;
+	
+	/*public VIPCustomer() {
+		
+		super(0, "no-name");
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+		
+		System.out.println("VIPCustomer() call");
+	}*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+	}
+	
+	// Override 재정의 된 부분
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		return price;
+	}
+
+	public String getAgentID() {
+		return agentID;
+	}
+}
